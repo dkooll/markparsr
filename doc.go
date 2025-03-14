6ee915687fd8bdd2be55b307992823728bc257a6
dkooll <dennis.kool@cloudnation.nl>
2025-03-14 13:52:41 +0100

    feat: remove docs.go

--- doc.go
+++
@@ -1,7 +0,0 @@
-/*
-
-This package validates documentation against Terraform module best practices. Also it's required to generate required sections by running terraform-docs with markdown document mode and inject output mode.
-For example terraform-docs markdown document --output-file README.md.new --output-mode inject .
-
-*/
-package markparsr
